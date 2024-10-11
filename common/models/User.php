e6154db95cc10d5efcbe3eba0d9f796829eb0b32
Ruba <ruba@souqfann.com>
2024-10-11 16:29:46 +0300

    create console command to add admin

--- common/models/User.php
+++ common/models/User.php
@@ -19,6 +19,7 @@ use yii\web\IdentityInterface;
  * @property string $email
  * @property string $auth_key
  * @property integer $status
+ * @property string $role
  * @property integer $created_at
  * @property integer $updated_at
  * @property string $password write-only password
@@ -31,8 +32,6 @@ class User extends ActiveRecord implements IdentityInterface
 
     const ROLE_ADMIN = 'admin';
     const ROLE_BUYER = 'buyer';
-
-    public $role; 
 
 
     /**
---
+++ console/controllers/SigninAdminController.php
@@ -0,0 +1,39 @@
+<?php
+namespace console\controllers;
+
+use Yii;
+use yii\console\Controller;
+use common\models\User;
+
+class SigninAdminController extends Controller
+{
+       /**
+     * Create a new admin user with the provided username, email, and password.
+     * 
+     * @param string $username The username of the admin user.
+     * @param string $email The email of the admin user.
+     * @param string $password The password for the admin user.
+     */
+    public function actionIndex($username, $email, $password)
+    {
+        if (User::findOne(['username' => $username]) || User::findOne(['email' => $email])) {
+            echo "A user with this username or email already exists.\n";
+            return;
+        }
+ 
+        $user = new User();
+        $user->username = $username;
+        $user->email = $email;
+        $user->setPassword($password);
+        $user->generateAuthKey();
+        $user->generateEmailVerificationToken();
+        $user->role = User::ROLE_ADMIN;
+
+        if ($user->save()) {
+            echo "Admin user created successfully.\n";
+        } else {
+            echo "Failed to create admin user. Errors:\n";
+            print_r($user->errors);
+        }
+    }
+}
